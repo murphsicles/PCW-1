8234bfc70e6d98cc6ebdd171611589fd826d425b
Roy Murphy <roycoins@gmail.com>
2025-08-31 07:22:12 +0100

    Update main.rs

--- benches/main.rs
+++ benches/main.rs
@@ -7,5 +7,10 @@ fn bench_split(c: &mut Criterion) {
     c.bench_function("bounded_split N~100", |b| b.iter(|| bounded_split(&scope, 1_000_000, 100, 10_000)));
 }
 
-criterion_group!(benches, bench_split);
+fn bench_derive_scalar(c: &mut Criterion) {
+    let scope = Scope::new([0;32], [0;32]);
+    c.bench_function("derive_scalar 1000 times", |b| b.iter(|| (0..1000).for_each(|i| derive_scalar(&scope, "recv", i).unwrap())));
+}
+
+criterion_group!(benches, bench_split, bench_derive_scalar);
 criterion_main!(benches);
