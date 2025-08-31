31dcb038234b73bc5f389afaa62bae8d2c0bfb87
Roy Murphy <roycoins@gmail.com>
2025-08-31 11:07:09 +0100

    Update main.rs

--- benches/main.rs
+++ benches/main.rs
@@ -1,15 +1,19 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 use pcw_protocol::scope::Scope;
 use pcw_protocol::split::bounded_split;
 
 fn bench_split(c: &mut Criterion) {
-    let scope = Scope::new([0;32], [0;32]);
-    c.bench_function("bounded_split N~100", |b| b.iter(|| bounded_split(&scope, 1_000_000, 100, 10_000)));
+    let scope = Scope::new([0; 32], [0; 32]);
+    c.bench_function("bounded_split N~100", |b| {
+        b.iter(|| bounded_split(&scope, 1_000_000, 100, 10_000))
+    });
 }
 
 fn bench_derive_scalar(c: &mut Criterion) {
-    let scope = Scope::new([0;32], [0;32]);
-    c.bench_function("derive_scalar 1000 times", |b| b.iter(|| (0..1000).for_each(|i| derive_scalar(&scope, "recv", i).unwrap())));
+    let scope = Scope::new([0; 32], [0; 32]);
+    c.bench_function("derive_scalar 1000 times", |b| {
+        b.iter(|| (0..1000).for_each(|i| derive_scalar(&scope, "recv", i).unwrap()))
+    });
 }
 
 criterion_group!(benches, bench_split, bench_derive_scalar);
