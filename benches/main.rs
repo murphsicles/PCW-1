12d75a27c0cc02aaf4e6753d9fb1f3004c8449da
Roy Murphy <roycoins@gmail.com>
2025-09-14 20:56:06 +0100

    Update main.rs

--- benches/main.rs
+++ benches/main.rs
@@ -3,16 +3,16 @@ use pcw_protocol::scope::Scope;
 use pcw_protocol::split::bounded_split;
 
 fn bench_split(c: &mut Criterion) {
-    let scope = Scope::new([0; 32], [0; 32]);
+    let scope = Scope::new([1u8; 32], [2u8; 32]).expect("Valid scope");
     c.bench_function("bounded_split N~100", |b| {
         b.iter(|| bounded_split(&scope, 1_000_000, 100, 10_000))
     });
 }
 
 fn bench_derive_scalar(c: &mut Criterion) {
-    let scope = Scope::new([0; 32], [0; 32]);
+    let scope = Scope::new([1u8; 32], [2u8; 32]).expect("Valid scope");
     c.bench_function("derive_scalar 1000 times", |b| {
-        b.iter(|| (0..1000).for_each(|i| derive_scalar(&scope, "recv", i).unwrap()))
+        b.iter(|| (0..1000).for_each(|i| scope.derive_scalar("recv", i).unwrap()))
     });
 }
 
