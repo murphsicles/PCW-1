cbfaf90be12b531606fcfe3e5eefac3e2f14ade6
Roy Murphy <roycoins@gmail.com>
2025-09-17 00:40:09 +0100

    Update protocol.rs

--- src/protocol.rs
+++ src/protocol.rs
@@ -17,7 +17,7 @@ pub fn ecdh_z(my_priv: &[u8; 32], their_pub: &PublicKey) -> Result<[u8; 32], Pcw
     let secp = Secp256k1::new();
     let secret_key = SecretKey::from_byte_array(*my_priv)?;
     // Convert SecretKey to Scalar for mul_tweak
-    let scalar = Scalar::from(&secret_key);
+    let scalar = Scalar::from(secret_key);
     let shared_point = their_pub.mul_tweak(&secp, &scalar)?;
     Ok(shared_point
         .serialize()
