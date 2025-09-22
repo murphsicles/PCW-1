e7d186906c8a5da22b16a12879081dab2bebb4ee
Roy Murphy <roycoins@gmail.com>
2025-09-22 22:16:42 +0100

    Update policy.rs

--- src/policy.rs
+++ src/policy.rs
@@ -88,7 +88,7 @@ impl Policy {
         let hash = sha256(&bytes);
         let msg = Message::from_digest(hash);
         let secp = Secp256k1::new();
-        let sig = secp.sign_ecdsa(msg, &SecretKey::from_byte_array(&key.priv_key)?);
+        let sig = secp.sign_ecdsa(msg, &SecretKey::from_byte_array(key.priv_key)?);
         self.sig_key = sig_key;
         self.sig_alg = sig_alg;
         self.sig = hex::encode(sig.serialize_der());
@@ -108,11 +108,11 @@ impl Policy {
         let bytes = canonical_json(&value)?;
         let hash = sha256(&bytes);
         let msg = Message::from_digest(hash);
-        let secp = Secp256k1::new();
         let pub_key = PublicKey::from_slice(&hex::decode(&self.sig_key)?)
             .map_err(|_| PcwError::Other("Invalid signature".to_string()))?;
         let sig = Signature::from_der(&hex::decode(&self.sig)?)
             .map_err(|_| PcwError::Other("Invalid signature".to_string()))?;
+        let secp = Secp256k1::new();
         secp.verify_ecdsa(msg, &sig, &pub_key)
             .map_err(|_| PcwError::Other("Invalid signature".to_string()))?;
         Ok(())
